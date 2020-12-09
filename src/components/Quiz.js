ef0d35160ccfaa4637ff58e6b79953e5c6dd9b2e
meeshcodile <ezekielmisheal4@gmail.com>
2020-12-09 13:30:32 +0100

    first commit

---
+++ src/components/Quiz.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles.css';
+import QuizData from './QuizData'
+
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Quiz
