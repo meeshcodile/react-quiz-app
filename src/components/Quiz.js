e29a401dd58e596c556fd4a4d4494752a2082ac2
meeshcodile <ezekielmisheal4@gmail.com>
2020-12-16 02:10:26 +0100

    finished part 1

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -83,9 +83,44 @@ class Quiz extends Component {
 
     render() {
         const {question, options, answer, quizEnd, currentIndex, userAnswer} = this.state
+
+        if(quizEnd){
+            return(
+                <div>
+                    <h1>quiz over , final score is {this.state.score}</h1>
+                    <p>the correct answer for the quiz are</p>
+                    <ul>
+                        {QuizData.map((item,index)=>(
+                            <li className='options' key={index}>
+                                {item.answer}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )
+        }
         return (
             <div>
                 <h2>{question}</h2>
+                <span>{`Question ${currentIndex + 1} of ${QuizData.length}`}</span>
+                {
+                    options.map(option =>
+                        <p key={option.id} className={`options ${userAnswer === option ? "selected": null}`} onClick={()=>this.checkAnswer(option)}>
+                            {option}
+                        </p>
+                        )
+                }
+
+                {currentIndex < QuizData.length -1 && 
+                <button disabled={this.state.disabled} onClick={this.nextQuestionHandler}>
+                    Next Question
+                </button>}
+
+                {currentIndex == QuizData.length -1 &&
+                <button onClick={this.finishHandler}>
+                    Finish
+                    </button>}
+
             </div>
         )
     }
